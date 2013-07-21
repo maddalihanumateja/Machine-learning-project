82bf1fe9a6ec6cf9201afaa6ee4eb867667dc610
Ryan Cook <cyanrook@gmail.com>
2013-07-21 19:52:05 -0400

    Added docs

--- Company_Combine.py
+++ Company_Combine.py
@@ -19,32 +19,18 @@ for filename in os.listdir(combine_dir):
         data_4.append(r2write);
 with open('data_4_back_raw.csv','w') as csvfile:
     CSVWriter = csv.writer(csvfile);
-    CSVWriter.writerow(['Close Change n','Low Change n','High Change n','Twitter n','Close Change n-1','Low Change n-1','High Change n-1','Twitter n-1','Close Change n-2','Low Change n-2','High Change n-2','Twitter n-2','Close Change n-3','Low Change n-3','High Change n-3','Twitter n-3','Close Change n-4','Low Change n-4','High Change n-4','Twitter n-4'])
+    CSVWriter.writerow(['Close Change n','Low Change n','High Change n','Twitter n','Close Change n-1','Low Change n-1','High Change n-1','Twitter n-1','Close Change n-2','Low Change n-2','High Change n-2','Twitter n-2','Close Change n-3','Low Change n-3','High Change n-3','Twitter n-3','Close Change n-4','Low Change n-4','High Change n-4','Twitter n-4','Close Change n-5','Low Change n-5','High Change n-5','Twitter n-5'])
     for row in data_4:
         CSVWriter.writerow(row);
 with open('data_4_back_classes.csv','w') as csvfile:
     CSVWriter = csv.writer(csvfile);
-    CSVWriter.writerow(['Class','Close Change n','Low Change n','High Change n','Twitter n','Close Change n-1','Low Change n-1','High Change n-1','Twitter n-1','Close Change n-2','Low Change n-2','High Change n-2','Twitter n-2','Close Change n-3','Low Change n-3','High Change n-3','Twitter n-3','Close Change n-4','Low Change n-4','High Change n-4','Twitter n-4'])
+    CSVWriter.writerow(['Class','Close Change n','Low Change n','High Change n','Twitter n','Close Change n-1','Low Change n-1','High Change n-1','Twitter n-1','Close Change n-2','Low Change n-2','High Change n-2','Twitter n-2','Close Change n-3','Low Change n-3','High Change n-3','Twitter n-3','Close Change n-4','Low Change n-4','High Change n-4','Twitter n-4','Close Change n-5','Low Change n-5','High Change n-5','Twitter n-5'])
     for row in data_4:
         r2write = [];
-        if (row[0] < -0.01):
-            r2write.append('(-inf:-1.0%)')
-        elif (row[0] < -0.007):
-            r2write.append('[-1.0%:-0.7%)')
-        elif (row[0] < -0.004):
-            r2write.append('[-0.7%:-0.4%)')
-        elif (row[0] < -0.001):
-            r2write.append('[-0.4%:-0.1%)')
-        elif (row[0] < 0.001):
-            r2write.append('[-0.1%:0.1%)')
-        elif (row[0] < 0.004):
-            r2write.append('[0.1%:0.4%)')
-        elif (row[0] < 0.007):
-            r2write.append('[0.4%:0.7%)')
-        elif (row[0] < 0.01):
-            r2write.append('[0.7%:1.0%)')
+        if (row[0] < 0):
+            r2write.append('down')
         else:
-            r2write.append('[1.0%:inf)')
+            r2write.append('up')
         r2write = r2write+row;
         CSVWriter.writerow(r2write);
 ## Set up for 3 days
@@ -64,32 +50,18 @@ for filename in os.listdir(combine_dir):
         data_3.append(r2write[4:]);
 with open('data_3_back_raw.csv','w') as csvfile:
     CSVWriter = csv.writer(csvfile);
-    CSVWriter.writerow(['Close Change n','Low Change n','High Change n','Twitter n','Close Change n-1','Low Change n-1','High Change n-1','Twitter n-1','Close Change n-2','Low Change n-2','High Change n-2','Twitter n-2','Close Change n-3','Low Change n-3','High Change n-3','Twitter n-3'])
+    CSVWriter.writerow(['Close Change n','Low Change n','High Change n','Twitter n','Close Change n-1','Low Change n-1','High Change n-1','Twitter n-1','Close Change n-2','Low Change n-2','High Change n-2','Twitter n-2','Close Change n-3','Low Change n-3','High Change n-3','Twitter n-3','Close Change n-4','Low Change n-4','High Change n-4','Twitter n-4'])
     for row in data_3:
         CSVWriter.writerow(row);
 with open('data_3_back_classes.csv','w') as csvfile:
     CSVWriter = csv.writer(csvfile);
-    CSVWriter.writerow(['Class','Close Change n','Low Change n','High Change n','Twitter n','Close Change n-1','Low Change n-1','High Change n-1','Twitter n-1','Close Change n-2','Low Change n-2','High Change n-2','Twitter n-2','Close Change n-3','Low Change n-3','High Change n-3','Twitter n-3'])
+    CSVWriter.writerow(['Class','Close Change n','Low Change n','High Change n','Twitter n','Close Change n-1','Low Change n-1','High Change n-1','Twitter n-1','Close Change n-2','Low Change n-2','High Change n-2','Twitter n-2','Close Change n-3','Low Change n-3','High Change n-3','Twitter n-3','Close Change n-4','Low Change n-4','High Change n-4','Twitter n-4'])
     for row in data_3:
         r2write = [];
-        if (row[0] < -0.01):
-            r2write.append('(-inf:-1.0%)')
-        elif (row[0] < -0.007):
-            r2write.append('[-1.0%:-0.7%)')
-        elif (row[0] < -0.004):
-            r2write.append('[-0.7%:-0.4%)')
-        elif (row[0] < -0.001):
-            r2write.append('[-0.4%:-0.1%)')
-        elif (row[0] < 0.001):
-            r2write.append('[-0.1%:0.1%)')
-        elif (row[0] < 0.004):
-            r2write.append('[0.1%:0.4%)')
-        elif (row[0] < 0.007):
-            r2write.append('[0.4%:0.7%)')
-        elif (row[0] < 0.01):
-            r2write.append('[0.7%:1.0%)')
+        if (row[0] < 0):
+            r2write.append('down')
         else:
-            r2write.append('[1.0%:inf)')
+            r2write.append('up')
         r2write = r2write+row;
         CSVWriter.writerow(r2write);
 
@@ -111,32 +83,18 @@ for filename in os.listdir(combine_dir):
         data_2.append(r2write[8:]);
 with open('data_2_back_raw.csv','w') as csvfile:
     CSVWriter = csv.writer(csvfile);
-    CSVWriter.writerow(['Close Change n','Low Change n','High Change n','Twitter n','Close Change n-1','Low Change n-1','High Change n-1','Twitter n-1','Close Change n-2','Low Change n-2','High Change n-2','Twitter n-2'])
+    CSVWriter.writerow(['Close Change n','Low Change n','High Change n','Twitter n','Close Change n-1','Low Change n-1','High Change n-1','Twitter n-1','Close Change n-2','Low Change n-2','High Change n-2','Twitter n-2','Close Change n-3','Low Change n-3','High Change n-3','Twitter n-3'])
     for row in data_2:
         CSVWriter.writerow(row);
 with open('data_2_back_classes.csv','w') as csvfile:
     CSVWriter = csv.writer(csvfile);
-    CSVWriter.writerow(['Class','Close Change n','Low Change n','High Change n','Twitter n','Close Change n-1','Low Change n-1','High Change n-1','Twitter n-1','Close Change n-2','Low Change n-2','High Change n-2','Twitter n-2'])
+    CSVWriter.writerow(['Class','Close Change n','Low Change n','High Change n','Twitter n','Close Change n-1','Low Change n-1','High Change n-1','Twitter n-1','Close Change n-2','Low Change n-2','High Change n-2','Twitter n-2','Close Change n-3','Low Change n-3','High Change n-3','Twitter n-3'])
     for row in data_2:
         r2write = [];
-        if (row[0] < -0.01):
-            r2write.append('(-inf:-1.0%)')
-        elif (row[0] < -0.007):
-            r2write.append('[-1.0%:-0.7%)')
-        elif (row[0] < -0.004):
-            r2write.append('[-0.7%:-0.4%)')
-        elif (row[0] < -0.001):
-            r2write.append('[-0.4%:-0.1%)')
-        elif (row[0] < 0.001):
-            r2write.append('[-0.1%:0.1%)')
-        elif (row[0] < 0.004):
-            r2write.append('[0.1%:0.4%)')
-        elif (row[0] < 0.007):
-            r2write.append('[0.4%:0.7%)')
-        elif (row[0] < 0.01):
-            r2write.append('[0.7%:1.0%)')
+        if (row[0] < 0):
+            r2write.append('down')
         else:
-            r2write.append('[1.0%:inf)')
+            r2write.append('up')
         r2write = r2write+row;
         CSVWriter.writerow(r2write);
 
@@ -159,31 +117,17 @@ for filename in os.listdir(combine_dir):
         data_1.append(r2write[12:]);
 with open('data_1_back_raw.csv','w') as csvfile:
     CSVWriter = csv.writer(csvfile);
-    CSVWriter.writerow(['Close Change n','Low Change n','High Change n','Twitter n','Close Change n-1','Low Change n-1','High Change n-1','Twitter n-1'])
+    CSVWriter.writerow(['Close Change n','Low Change n','High Change n','Twitter n','Close Change n-1','Low Change n-1','High Change n-1','Twitter n-1','Close Change n-2','Low Change n-2','High Change n-2','Twitter n-2'])
     for row in data_1:
         CSVWriter.writerow(row);
 with open('data_1_back_classes.csv','w') as csvfile:
     CSVWriter = csv.writer(csvfile);
-    CSVWriter.writerow(['Class','Close Change n','Low Change n','High Change n','Twitter n','Close Change n-1','Low Change n-1','High Change n-1','Twitter n-1'])
+    CSVWriter.writerow(['Class','Close Change n','Low Change n','High Change n','Twitter n','Close Change n-1','Low Change n-1','High Change n-1','Twitter n-1','Close Change n-2','Low Change n-2','High Change n-2','Twitter n-2'])
     for row in data_1:
         r2write = [];
-        if (row[0] < -0.01):
-            r2write.append('(-inf:-1.0%)')
-        elif (row[0] < -0.007):
-            r2write.append('[-1.0%:-0.7%)')
-        elif (row[0] < -0.004):
-            r2write.append('[-0.7%:-0.4%)')
-        elif (row[0] < -0.001):
-            r2write.append('[-0.4%:-0.1%)')
-        elif (row[0] < 0.001):
-            r2write.append('[-0.1%:0.1%)')
-        elif (row[0] < 0.004):
-            r2write.append('[0.1%:0.4%)')
-        elif (row[0] < 0.007):
-            r2write.append('[0.4%:0.7%)')
-        elif (row[0] < 0.01):
-            r2write.append('[0.7%:1.0%)')
+        if (row[0] < 0):
+            r2write.append('down')
         else:
-            r2write.append('[1.0%:inf)')
+            r2write.append('up')
         r2write = r2write+row;
         CSVWriter.writerow(r2write);
